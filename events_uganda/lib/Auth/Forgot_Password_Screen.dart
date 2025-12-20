1d8027e863d866d223d064915f0c6ff2ea5857c3
Alvin11David <alvin69david@gmail.com>
2025-12-20 19:05:00 +0300

    RSS

--- events_uganda/lib/Auth/Forgot_Password_Screen.dart
+++ events_uganda/lib/Auth/Forgot_Password_Screen.dart
@@ -100,7 +100,6 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
 
           ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(
-              
               backgroundColor: Color(0xFF1BCC94),
               content: Text(
                 'OTP sent to your phone',
