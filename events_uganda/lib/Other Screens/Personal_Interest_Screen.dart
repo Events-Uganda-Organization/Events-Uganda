da727a0339edd4bd03deb50a2691ff8e41da80a8
Alvin11David <alvin69david@gmail.com>
2025-12-21 10:58:08 +0300

    RSS

--- events_uganda/lib/Other Screens/Personal_Interest_Screen.dart
+++ events_uganda/lib/Other Screens/Personal_Interest_Screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-/// Personal interests entry screen with full-bleed background artwork.
 class PersonalInterestScreen extends StatelessWidget {
   const PersonalInterestScreen({super.key});
 
