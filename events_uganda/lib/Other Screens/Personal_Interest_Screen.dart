1462a5cb142f1bd1f8955e1a4d048b22d3fb5772
Alvin11David <alvin69david@gmail.com>
2025-12-21 16:29:16 +0300

    RSS

--- events_uganda/lib/Other Screens/Personal_Interest_Screen.dart
+++ events_uganda/lib/Other Screens/Personal_Interest_Screen.dart
@@ -151,6 +151,7 @@ class PersonalInterestScreen extends StatelessWidget {
                       ),
                     ),
                   ),
+                  SizedBox(height: screenHeight * 0.04,),
                 ],
               ),
             ),
