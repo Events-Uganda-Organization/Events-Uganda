936c98dd245ff39d3dcdcc7c5b2206476ba00c4e
Alvin11David <alvin69david@gmail.com>
2025-12-22 18:28:08 +0300

    RSS

--- events_uganda/lib/Users/Customers/Service_Listing_Screen.dart
+++ events_uganda/lib/Users/Customers/Service_Listing_Screen.dart
@@ -948,7 +948,7 @@ class _ServiceListingScreenState extends State<ServiceListingScreen>
               ),
             ),
             Positioned(
-              top: screenHeight * 0.001,
+              top: screenHeight * 0.009,
               right: -screenWidth * 0.1,
               child: Image.asset(
                 'assets/images/chicken.png',
