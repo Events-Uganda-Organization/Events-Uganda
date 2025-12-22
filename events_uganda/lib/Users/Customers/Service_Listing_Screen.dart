df7eb0f6d19ac92639b221011e78337f4e58f05f
Alvin11David <alvin69david@gmail.com>
2025-12-22 19:18:06 +0300

    Added the verified badge

--- events_uganda/lib/Users/Customers/Service_Listing_Screen.dart
+++ events_uganda/lib/Users/Customers/Service_Listing_Screen.dart
@@ -1191,7 +1191,7 @@ class _ServiceListingScreenState extends State<ServiceListingScreen>
               left: screenWidth * 0.04,
               child: Container(
                 width: screenWidth * 0.34,
-                height: screenHeight * 0.045,
+                height: screenHeight * 0.046,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(30),
