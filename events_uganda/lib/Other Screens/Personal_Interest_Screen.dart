260fda281bb79dfca3e6930f7e3aec3be77706e3
Alvin11David <alvin69david@gmail.com>
2025-12-29 21:07:32 +0300

    Working on links

--- events_uganda/lib/Other Screens/Personal_Interest_Screen.dart
+++ events_uganda/lib/Other Screens/Personal_Interest_Screen.dart
@@ -134,21 +134,7 @@ class _PersonalInterestScreenState extends State<PersonalInterestScreen> {
             ),
 
             // Title text below the bar
-            Positioned(
-              top: size.height * 0.05,
-              left: 0,
-              right: 0,
-              child: Text(
-                'Time To Choose',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Montserrat',
-                  fontSize: size.width * 0.08,
-                  fontWeight: FontWeight.w800,
-                ),
-              ),
-            ),
+            
 
             // Subtitle text
             Positioned(
