d63131ac918796ed9a08e59d1653a549bbecbf29
Alvin11David <alvin69david@gmail.com>
2025-12-29 21:35:54 +0300

    Working on links

--- events_uganda/lib/Other Screens/Personal_Interest_Screen.dart
+++ events_uganda/lib/Other Screens/Personal_Interest_Screen.dart
@@ -216,7 +216,7 @@ class _PersonalInterestScreenState extends State<PersonalInterestScreen> {
                     child: Column(
                       children: [
                         Text(
-                          "Sign In",
+                          "Tell us what you are most interested\nin to help us deliver the best to you ",
                           textAlign: TextAlign.center,
                           style: TextStyle(
                             fontFamily: 'Montserrat',
