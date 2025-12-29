2aa2464862ff37580e96ca445bc8562bdb5854d0
Alvin11David <alvin69david@gmail.com>
2025-12-29 21:15:42 +0300

    Working on links

--- events_uganda/lib/Other Screens/Personal_Interest_Screen.dart
+++ events_uganda/lib/Other Screens/Personal_Interest_Screen.dart
@@ -165,185 +165,171 @@ class _PersonalInterestScreenState extends State<PersonalInterestScreen> {
               ),
             ),
             Padding(
-                      padding: EdgeInsets.only(
-                        top: screenHeight * 0.00,
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                      ),
-                      child: SingleChildScrollView(
-                        child: ConstrainedBox(
-                          constraints: BoxConstraints(
-                            minHeight: screenHeight * 1.2,
+              padding: EdgeInsets.only(
+                top: screenHeight * 0.00,
+                left: 0,
+                right: 0,
+                bottom: 0,
+              ),
+              child: SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: screenHeight * 1.2),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 20),
+
+                      // Three horizontal dots
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 15,
+                            height: 10,
+                            decoration: BoxDecoration(
+                              color: const Color(0xFF825E34).withOpacity(0.3),
+                              shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
                           ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
+                          const SizedBox(width: 12),
+                          Container(
+                            width: 18,
+                            height: 10,
+                            decoration: BoxDecoration(
+                              color: Color(0xFF825E34),
+                              shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                          const SizedBox(width: 12),
+                          Container(
+                            width: 15,
+                            height: 10,
+                            decoration: BoxDecoration(
+                              color: const Color(0xFF825E34).withOpacity(0.3),
+                              shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(height: screenHeight * 0.03),
+
+                      // Large mail icon
+                      Container(
+                        padding: const EdgeInsets.all(20),
+                        decoration: const BoxDecoration(
+                          color: Color(0xFF825E34),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(50),
+                            topRight: Radius.circular(50),
+                            bottomRight: Radius.circular(50),
+                            bottomLeft: Radius.circular(0),
+                          ),
+                        ),
+                        child: const Icon(
+                          Icons.mail,
+                          size: 50,
+                          color: Colors.black,
+                        ),
+                      ),
+
+                      SizedBox(height: screenHeight * 0.03),
+
+                      SizedBox(height: screenHeight * 0.04),
+
+                      SizedBox(height: screenHeight * 0.05),
+
+                      GestureDetector(
+                        child: Container(
+                          width: screenWidth * 0.5,
+                          height: screenHeight * 0.06,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                const Color(0xFFE0E7FF),
+                                const Color(0xFFD59A00),
+                              ],
+
+                              stops: [0.0, 0.47],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                            border: Border.all(
+                              color: Color(0xFF825E34),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              const SizedBox(height: 20),
-
-                              // Three horizontal dots
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    width: 15,
-                                    height: 10,
-                                    decoration: BoxDecoration(
-                                      color: const Color(
-                                        0xFF825E34,
-                                      ).withOpacity(0.3),
-                                      shape: BoxShape.rectangle,
-                                      borderRadius: BorderRadius.circular(5),
-                                    ),
-                                  ),
-                                  const SizedBox(width: 12),
-                                  Container(
-                                    width: 18,
-                                    height: 10,
-                                    decoration: BoxDecoration(
-                                      color: Color(0xFF825E34),
-                                      shape: BoxShape.rectangle,
-                                      borderRadius: BorderRadius.circular(5),
-                                    ),
-                                  ),
-                                  const SizedBox(width: 12),
-                                  Container(
-                                    width: 15,
-                                    height: 10,
-                                    decoration: BoxDecoration(
-                                      color: const Color(
-                                        0xFF825E34,
-                                      ).withOpacity(0.3),
-                                      shape: BoxShape.rectangle,
-                                      borderRadius: BorderRadius.circular(5),
-                                    ),
-                                  ),
-                                ],
+                              Icon(
+                                Icons.refresh,
+                                color: Colors.white,
+                                size: screenWidth * 0.06,
                               ),
-
-                              SizedBox(height: screenHeight * 0.03),
-
-                              // Large mail icon
-                              Container(
-                                padding: const EdgeInsets.all(20),
-                                decoration: const BoxDecoration(
-                                  color: Color(0xFF825E34),
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(50),
-                                    topRight: Radius.circular(50),
-                                    bottomRight: Radius.circular(50),
-                                    bottomLeft: Radius.circular(0),
-                                  ),
-                                ),
-                                child: const Icon(
-                                  Icons.mail,
-                                  size: 50,
-                                  color: Colors.black,
+                              SizedBox(width: screenWidth * 0.03),
+                              Text(
+                                _countdown > 0 ? '$_countdown' : 'Resend',
+                                style: TextStyle(
+                                  fontSize: screenWidth * 0.05,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontFamily: 'Poppins',
                                 ),
                               ),
-
-                              SizedBox(height: screenHeight * 0.03),
-
-                      
-
-                              SizedBox(height: screenHeight * 0.04),
-
-
-                              SizedBox(height: screenHeight * 0.05),
-
-                              GestureDetector(
-                                child: Container(
-                                  width: screenWidth * 0.5,
-                                  height: screenHeight * 0.06,
-                                  decoration: BoxDecoration(
-                                    gradient: LinearGradient(
-                                      colors: [
-                                              const Color(0xFFE0E7FF),
-                                              const Color(0xFFD59A00),
-                                            ]
-                                          : [
-                                              Colors.grey[300]!,
-                                              Colors.grey[500]!,
-                                            ],
-                                      stops: [0.0, 0.47],
-                                      begin: Alignment.topLeft,
-                                      end: Alignment.bottomRight,
-                                    ),
-                                    border: Border.all(
-                                      color: Color(0xFF825E34),
-                                      width: 1,
-                                    ),
-                                    borderRadius: BorderRadius.circular(30),
-                                  ),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Icon(
-                                        Icons.refresh,
-                                        color: Colors.white,
-                                        size: screenWidth * 0.06,
-                                      ),
-                                      SizedBox(width: screenWidth * 0.03),
-                                      Text(
-                                        _countdown > 0
-                                            ? '$_countdown'
-                                            : 'Resend',
-                                        style: TextStyle(
-                                          fontSize: screenWidth * 0.05,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
-                                          fontFamily: 'Poppins',
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-
-                              SizedBox(height: screenHeight * 0.04),
-
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'Already have an account? ',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: screenWidth * 0.038,
-                                      fontFamily: 'Montserrat',
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                  GestureDetector(
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => SignInScreen(),
-                                        ),
-                                      );
-                                    },
-                                    child: Text(
-                                      'Sign In',
-                                      style: TextStyle(
-                                        color: const Color(0xFFD59A00),
-                                        fontSize: screenWidth * 0.040,
-                                        fontFamily: 'Montserrat',
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-
-                              SizedBox(height: screenHeight * 0.05),
                             ],
                           ),
                         ),
                       ),
-                    ),
 
-                        // Glassy rectangles row
+                      SizedBox(height: screenHeight * 0.04),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Already have an account? ',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: screenWidth * 0.038,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => SignInScreen(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              'Sign In',
+                              style: TextStyle(
+                                color: const Color(0xFFD59A00),
+                                fontSize: screenWidth * 0.040,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(height: screenHeight * 0.05),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            // Glassy rectangles row
             Positioned(
               top: size.height * 0.30,
               left: size.width * 0.04,
