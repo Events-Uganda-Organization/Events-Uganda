0983489bc74fc4838dd02e2985e9eb2a3003e277
Alvin11David <alvin69david@gmail.com>
2025-12-31 20:03:14 +0300

    LAST COMMITOF 2025 WE CALL IT A YEAR

--- events_uganda/lib/Users/Customers/Service_Details_Screen.dart
+++ events_uganda/lib/Users/Customers/Service_Details_Screen.dart
@@ -841,7 +841,7 @@ class _ServiceDetailsScreenState extends State<ServiceDetailsScreen>
                         ),
                       ),
                       Positioned(
-                        top: screenHeight * 0.78 - offset,
+                        top: screenHeight * 0.76 - offset,
                         left: screenWidth * 0.02,
                         right: screenWidth * 0.02,
                         child: Container(
