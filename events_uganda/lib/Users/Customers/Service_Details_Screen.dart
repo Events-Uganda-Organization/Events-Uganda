637e349b643bee55983d8eb78cfa5bdf4a189363
Alvin11David <alvin69david@gmail.com>
2025-12-31 18:11:24 +0300

    Working on the profile screen

--- events_uganda/lib/Users/Customers/Service_Details_Screen.dart
+++ events_uganda/lib/Users/Customers/Service_Details_Screen.dart
@@ -841,7 +841,7 @@ class _ServiceDetailsScreenState extends State<ServiceDetailsScreen>
                         ),
                       ),
                       Positioned(
-                        top: screenHeight * 0.75 - offset,
+                        top: screenHeight * 0.82 - offset,
                         left: screenWidth * 0.02,
                         right: screenWidth * 0.02,
                         child: Container(
