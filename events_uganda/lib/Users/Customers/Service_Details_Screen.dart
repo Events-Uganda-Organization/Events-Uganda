007e3439e62e52c90591d5d0e4387ebe3e9ea14d
Alvin11David <alvin69david@gmail.com>
2026-01-01 17:16:38 +0300

    HAPPY NEW YEAR

--- events_uganda/lib/Users/Customers/Service_Details_Screen.dart
+++ events_uganda/lib/Users/Customers/Service_Details_Screen.dart
@@ -340,7 +340,7 @@ class _ServiceDetailsScreenState extends State<ServiceDetailsScreen>
                       ),
                       Positioned(
                         top:
-                            screenHeight * 0.57 -
+                            screenHeight * 0.56 -
                             offset, // Provider name sits above
                         left: screenWidth * 0.03,
                         right: screenWidth * 0.03,
