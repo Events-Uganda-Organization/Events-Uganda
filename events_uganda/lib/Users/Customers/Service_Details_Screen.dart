d12c488eecb0ebf3d77ee4d99bd6e0586a4fe834
Alvin11David <alvin69david@gmail.com>
2026-01-01 17:25:08 +0300

    HAPPY NEW YEAR

--- events_uganda/lib/Users/Customers/Service_Details_Screen.dart
+++ events_uganda/lib/Users/Customers/Service_Details_Screen.dart
@@ -1335,8 +1335,8 @@ class _ServiceDetailsScreenState extends State<ServiceDetailsScreen>
                       // ===== Reviews & Ratings Section in nested cards =====
                       Positioned(
                         top: _showReviewSection
-                            ? screenHeight * 1.15 - offset
-                            : screenHeight * 1.52 - offset,
+                            ? screenHeight * 1.48 - offset
+                            : screenHeight * 1.38 - offset,
                         left: screenWidth * 0.022,
                         right: screenWidth * 0.022,
                         child: Container(
