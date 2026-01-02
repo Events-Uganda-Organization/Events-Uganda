e7233caee1300650258c6043dd9b15e5b51211ea
Alvin11David <alvin69david@gmail.com>
2026-01-02 19:05:20 +0300

    Added navigation to the profile screen

--- events_uganda/web/firebase-messaging-sw.js
+++ events_uganda/web/firebase-messaging-sw.js
@@ -1,29 +1,32 @@
-importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js"
+);
 
 // Initialize Firebase in the service worker
-firebase.initializeApp({
-  apiKey: "AIzaSyBw7LZQY4Y4z4z4z4z4z4z4z4z4z4z4z4",
-  authDomain: "events-uganda.firebaseapp.com",
-  projectId: "events-uganda",
-  storageBucket: "events-uganda.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcd1234"
-});
+if (!firebase.apps.length) {
+  firebase.initializeApp({});
+}
 
 // Get messaging instance
 const messaging = firebase.messaging();
 
 // Handle background messages
-messaging.onBackgroundMessage(function(payload) {
+messaging.onBackgroundMessage(function (payload) {
   console.log("Received background message ", payload);
-  const notificationTitle = payload.notification.title;
+  const notificationTitle = payload.notification?.title || "New Notification";
   const notificationOptions = {
-    body: payload.notification.body,
-    icon: "/icons/icon-192x192.png"
+    body: payload.notification?.body || "",
+    icon: "/icons/icon-192x192.png",
   };
 
   return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
     notificationTitle,
     notificationOptions
   );
