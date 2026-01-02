6537c729ef2e7fc1b0a0ef0d0e88cde2a2d8a641
Alvin11David <alvin69david@gmail.com>
2026-01-02 12:58:13 +0300

    Added navigation to the profile screen

--- events_uganda/lib/Users/Customers/Service_Listing_Screen.dart
+++ events_uganda/lib/Users/Customers/Service_Listing_Screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:ui';
+import 'package:events_uganda/Users/Customers/Customer_Profile_Screen.dart';
 import 'package:flutter/material.dart';
 import 'package:events_uganda/components/Bottom_Navbar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
