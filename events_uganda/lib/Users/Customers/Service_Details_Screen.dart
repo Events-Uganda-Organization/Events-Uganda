b5bf7270d782905db137632fb98bbb0496fc5299
Alvin11David <alvin69david@gmail.com>
2026-01-02 15:55:28 +0300

    Added navigation to the profile screen

--- events_uganda/lib/Users/Customers/Service_Details_Screen.dart
+++ events_uganda/lib/Users/Customers/Service_Details_Screen.dart
@@ -1216,7 +1216,7 @@ class _ServiceDetailsScreenState extends State<ServiceDetailsScreen>
                         ),
                       ),
                       Positioned(
-                        top: screenHeight * 1.13 - offset,
+                        top: screenHeight * 1.16 - offset,
                         left: screenWidth * 0.02,
                         right: screenWidth * 0.02,
                         child: Column(
