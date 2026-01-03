66f641a1206ea1f15908a8550d86274168686b58
Alvin11David <alvin69david@gmail.com>
2026-01-03 21:03:44 +0300

    Added navigation to the profile screen

--- events_uganda/lib/Users/Customers/All_Categories_Screen.dart
+++ events_uganda/lib/Users/Customers/All_Categories_Screen.dart
@@ -1067,7 +1067,6 @@ class _AllCategoriesScreenState extends State<AllCategoriesScreen>
     final screenHeight = MediaQuery.of(context).size.height;
     final promoTop =
         screenHeight * 0.19 + screenWidth * 0.12 + screenHeight * 0.02;
-    final promoHeight = screenWidth * 0.46;
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
