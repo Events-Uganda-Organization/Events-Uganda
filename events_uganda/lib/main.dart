feab7559a82ba456f5382b40bf467504994660ab
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-01-04 12:28:18 +0300

    Added the Catering Listing Screen file

--- events_uganda/lib/main.dart
+++ events_uganda/lib/main.dart
@@ -5,11 +5,19 @@ import 'package:events_uganda/Auth/Reset_Password_Screen.dart';
 import 'package:events_uganda/Other%20Screens/Splash_Screen.dart';
 import 'package:events_uganda/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Enable App Check for phone authentication with debug token
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.playIntegrity,
+    androidDebugToken: '5A6C9262-7409-4D39-AECE-47B66161ABF1',
+  );
+
   runApp(const MyApp());
 }
 
@@ -29,6 +37,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
