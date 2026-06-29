61263e7735eeeebc04a49d05d9340d79c3f6892b
Alvin11David <alvin69david@gmail.com>
2026-06-29 10:42:37 +0300

    auto-commit: 2026-06-29 10:42:37

--- events_uganda/lib/Users/Date_Of_Booking_Screen.dart
+++ events_uganda/lib/Users/Date_Of_Booking_Screen.dart
@@ -71,26 +71,6 @@ class _DateOfBookingScreenState extends State<DateOfBookingScreen>
               ),
             ),
             Positioned(
-              top: screenHeight * 0.001,
-              left: -screenWidth * 0.1,
-              child: Image.asset(
-                'assets/images/chicken.png',
-                width: screenWidth * 0.4,
-                height: screenWidth * 0.4,
-                fit: BoxFit.contain,
-              ),
-            ),
-            Positioned(
-              top: screenHeight * 0.1,
-              right: -screenWidth * 0.1,
-              child: Image.asset(
-                'assets/images/chicken.png',
-                width: screenWidth * 0.4,
-                height: screenWidth * 0.4,
-                fit: BoxFit.contain,
-              ),
-            ),
-            Positioned(
               top: screenHeight * 0.03,
               left: screenWidth * 0.04,
               child: Container(
