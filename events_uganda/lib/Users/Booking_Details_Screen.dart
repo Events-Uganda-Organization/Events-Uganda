47ac5c847194e63ec3ec216c7ee06e8a4672de0b
atreavez <alvin69david@gmail.com>
2026-07-03 00:15:32 +0300

    auto-commit: 2026-07-03 00:15:32

--- events_uganda/lib/Users/Booking_Details_Screen.dart
+++ events_uganda/lib/Users/Booking_Details_Screen.dart
@@ -863,7 +863,7 @@ class _BookingDetailsScreenState extends State<BookingDetailsScreen>
                             left: (screenWidth - screenWidth * 0.95) / 2,
                             child: Container(
                               width: screenWidth * 0.95,
-                              height: screenWidth * 0.95 * (336 / 350) * 0.4,
+                               height: screenWidth * 0.95 * (336 / 350) * 0.55,
                               decoration: BoxDecoration(
                                 color: Colors.white,
                                 borderRadius: BorderRadius.circular(30),
