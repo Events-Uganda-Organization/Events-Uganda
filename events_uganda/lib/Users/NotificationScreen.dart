ced61ee1214cf252683bdc572c11488b7b4a9e9f
atreavez <alvin69david@gmail.com>
2026-07-04 01:25:44 +0300

    Update events_uganda/lib/Users/NotificationScreen.dart

--- events_uganda/lib/Users/NotificationScreen.dart
+++ events_uganda/lib/Users/NotificationScreen.dart
@@ -694,7 +694,7 @@ class _NotificationScreenState extends State<NotificationScreen>
                                     width: screenWidth * 0.128,
                                     height: screenWidth * 0.128,
                                     decoration: BoxDecoration(
-                                      color: const Color(0xFF55FF27).withValues(alpha: 0.3),
+                                      color: const Color(0xFF55FF27).withValues(alpha: 0.5),
                                       shape: BoxShape.circle,
                                     ),
                                     child: Center(
