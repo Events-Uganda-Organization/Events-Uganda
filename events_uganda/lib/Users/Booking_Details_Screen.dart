37212c9b64d230c7635b82887fed150160ec9001
atreavez <alvin69david@gmail.com>
2026-07-03 03:39:12 +0300

    auto-commit: 2026-07-03 03:39:12

--- events_uganda/lib/Users/Booking_Details_Screen.dart
+++ events_uganda/lib/Users/Booking_Details_Screen.dart
@@ -1118,7 +1118,7 @@ class _BookingDetailsScreenState extends State<BookingDetailsScreen>
                                     screenWidth * 0.95 * (336 / 350) * 0.7 +
                                     screenWidth * 0.58 +
                                     6 +
-                                     screenWidth * 0.55 +
+                                      screenWidth * 0.7 +
                                      6 +
                                      screenWidth * 0.95 * (336 / 350) * 0.65 +
                                      8,
