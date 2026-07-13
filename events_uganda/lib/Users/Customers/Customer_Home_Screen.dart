6a54e1121ee263b8e1c59999280d75aac296c085
Alvin11David <alvin69david@gmail.com>
2026-07-13 15:49:31 +0300

    auto-commit: 2026-07-13 15:49:31

--- events_uganda/lib/Users/Customers/Customer_Home_Screen.dart
+++ events_uganda/lib/Users/Customers/Customer_Home_Screen.dart
@@ -13,6 +13,7 @@ import 'package:events_uganda/Users/Customers/Service_Details_Screen.dart';
 import 'package:events_uganda/components/Bottom_Navbar.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:events_uganda/components/sidebar_menu.dart';
 import 'package:flutter/material.dart';
 
 class CustomerHomeScreen extends StatefulWidget {
