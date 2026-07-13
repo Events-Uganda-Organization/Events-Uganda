45f056067cda61d8780130e1a034773720b6eaa7
Alvin11David <alvin69david@gmail.com>
2026-07-13 22:08:59 +0300

    auto-commit: 2026-07-13 22:08:59

--- events_uganda/lib/components/sidebar_menu.dart
+++ events_uganda/lib/components/sidebar_menu.dart
@@ -178,7 +178,7 @@ class _SidebarPanel extends StatelessWidget {
                                 ),
                                 SizedBox(height: h * 0.02),
                                 Padding(
-                                    padding: EdgeInsets.symmetric(horizontal: w * 0.08),
+                                    padding: EdgeInsets.only(left: w * 0.046),
                                     child: Container(
                                         height: h * 0.035,
                                         decoration: BoxDecoration(
