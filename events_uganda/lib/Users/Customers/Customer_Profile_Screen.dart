dad63865ab70001c420c5885ede546013931f6ce
Alvin11David <alvin69david@gmail.com>
2026-07-18 23:30:59 +0300

    auto-commit: 2026-07-18 23:30:59

--- events_uganda/lib/Users/Customers/Customer_Profile_Screen.dart
+++ events_uganda/lib/Users/Customers/Customer_Profile_Screen.dart
@@ -864,14 +864,14 @@ class _CustomerProfileScreenState extends State<CustomerProfileScreen>
               padding: EdgeInsets.all(w * 0.04),
               decoration: BoxDecoration(
                 gradient: const LinearGradient(
-                  colors: [Color(0xFFE8C7B6), Color(0xFFCC471B)],
+                  colors: [Color(0xFFCE93D8), Color(0xFF7B1FA2)],
                   begin: Alignment.topLeft,
                   end: Alignment.bottomRight,
                 ),
                 borderRadius: BorderRadius.circular(16),
                 boxShadow: [
                   BoxShadow(
-                    color: const Color(0xFFCC471B).withValues(alpha: 0.3),
+                    color: const Color(0xFF7B1FA2).withValues(alpha: 0.3),
                     blurRadius: 12,
                     offset: const Offset(0, 4),
                   ),
