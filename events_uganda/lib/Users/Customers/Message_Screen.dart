d3332b84b2f873a101e7340b063c87f9eb4bf292
Alvin11David <alvin69david@gmail.com>
2026-08-01 13:13:42 +0300

    auto-commit: 2026-08-01 13:13:42

--- events_uganda/lib/Users/Customers/Message_Screen.dart
+++ events_uganda/lib/Users/Customers/Message_Screen.dart
@@ -151,6 +151,12 @@ class _MessageScreenState extends State<MessageScreen> {
                     color: const Color(0xFFCD7C20),
                     size: screenWidth * 0.06,
                   ),
+                  SizedBox(width: screenWidth * 0.03),
+                  Icon(
+                    Icons.more_vert,
+                    color: Colors.black,
+                    size: screenWidth * 0.06,
+                  ),
                 ],
               ),
             ),
