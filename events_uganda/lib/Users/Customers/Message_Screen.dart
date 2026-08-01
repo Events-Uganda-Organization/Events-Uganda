9e3a4becd2142c199cc2a639495f32de357c8d40
Alvin11David <alvin69david@gmail.com>
2026-08-01 13:46:56 +0300

    auto-commit: 2026-08-01 13:46:56

--- events_uganda/lib/Users/Customers/Message_Screen.dart
+++ events_uganda/lib/Users/Customers/Message_Screen.dart
@@ -243,7 +243,7 @@ class _MessageScreenState extends State<MessageScreen> {
                               shape: BoxShape.circle,
                             ),
                             child: Transform.rotate(
-                              angle: math.pi * 10 / 120,
+                              angle: math.pi * 100 / 400,
                               child: Icon(
                                 Icons.send,
                                 color: Colors.white,
