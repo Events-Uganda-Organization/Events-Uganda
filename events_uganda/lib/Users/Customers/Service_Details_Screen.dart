f21584b70205e97df90ab180502a0aaccd7f87b0
Alvin11David <alvin69david@gmail.com>
2026-08-16 09:04:38 +0300

    auto-commit: 2026-08-16 09:04:37

--- events_uganda/lib/Users/Customers/Service_Details_Screen.dart
+++ events_uganda/lib/Users/Customers/Service_Details_Screen.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:events_uganda/Users/Date_Of_Booking_Screen.dart';
 import 'package:flutter/material.dart';
 import 'package:events_uganda/Auth/auth_service.dart';
-import 'package:events_uganda/Users/NotificationScreen.dart';
+import 'package:events_uganda/Users/NotificationScreen.dart' hide ReviewModel;
 import 'package:events_uganda/components/Bottom_Navbar.dart';
 import 'package:events_uganda/Services/chat_service.dart';
 import 'package:events_uganda/Services/call_service.dart';
