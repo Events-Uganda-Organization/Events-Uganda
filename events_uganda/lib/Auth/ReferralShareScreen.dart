5ddb6fcd9f7ddf7873b23695fcef2b1e934f54ce
Alvin11David <alvin69david@gmail.com>
2026-08-21 08:01:23 +0300

    auto-commit: 2026-08-21 08:01:23

--- events_uganda/lib/Auth/ReferralShareScreen.dart
+++ events_uganda/lib/Auth/ReferralShareScreen.dart
@@ -152,6 +152,8 @@ class _ReferralShareScreenState extends State<ReferralShareScreen> {
                             ],
                           ),
                         ),
+                      ),
+                    ),
                       ],
                     );
                   },
