f227aad4d0b72f15fb714bc35c206636844f58d6
Alvin11David <alvin69david@gmail.com>
2026-08-23 22:05:46 +0300

    auto-commit: 2026-08-23 22:05:46

---
+++ events_uganda/lib/Users/Contact_Support_Screen.dart
@@ -0,0 +1,250 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:events_uganda/components/snackbar_helper.dart';
+import 'package:events_uganda/Users/NotificationScreen.dart';
+
+class ContactSupportScreen extends StatefulWidget {
+  const ContactSupportScreen({super.key});
+
+  @override
+  State<ContactSupportScreen> createState() => _ContactSupportScreenState();
+}
+
+class _ContactSupportScreenState extends State<ContactSupportScreen> {
+  Future<String> _getReferralCode() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('userReferralCode') ?? 'No referral code yet';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              bottom: 0,
+              right: (screenWidth + screenWidth) / 300,
+              child: Image.asset(
+                'assets/backgroundcolors/normalscreen.png',
+                width: screenWidth * 1.08,
+                height: screenHeight * 0.9,
+                fit: BoxFit.contain,
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.13,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: FutureBuilder<String>(
+                  future: _getReferralCode(),
+                  builder: (context, snapshot) {
+                    final referralCode = snapshot.data ?? 'Loading...';
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Share Your Code',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            fontSize: screenWidth * 0.048,
+                            color: Colors.black,
+                          ),
+                        ),
+                         SizedBox(height: screenHeight * 0.009),
+                         Text(
+                           'Invite friends & earn rewards',
+                           style: TextStyle(
+                             fontWeight: FontWeight.w500,
+                             fontSize: screenWidth * 0.035,
+                             color: Colors.black,
+                           ),
+                         ),
+                         SizedBox(height: screenHeight * 0.03),
+                        Image.asset(
+                          'assets/images/referralcode.png',
+                          width: screenWidth * 0.75,
+                          fit: BoxFit.contain,
+                        ),
+                        SizedBox(height: screenHeight * 0.02),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.08),
+                          child: Text(
+                            'Here is your referral code that you can share with friends and family who have not yet joined the app to win gifts.',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: screenWidth * 0.032,
+                              color: Colors.black87,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: screenHeight * 0.03),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: screenWidth * 0.1),
+                          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05, vertical: screenHeight * 0.015),
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFCD7C20),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    referralCode,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: screenWidth * 0.04,
+                                      color: Colors.white,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                GestureDetector(
+                                onTap: () {
+                                  Clipboard.setData(ClipboardData(text: referralCode));
+                                  SnackbarHelper.show(
+                                    context,
+                                    'Referral code copied!',
+                                    icon: Icons.check_circle_rounded,
+                                    backgroundColor: const Color(0xFFCD7C20),
+                                  );
+                                },
+                                  child: Icon(
+                                    Icons.copy,
+                                    color: Colors.white,
+                                    size: screenWidth * 0.05,
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.01,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Icon(
+                  Icons.card_giftcard,
+                  color: const Color(0xFFE94560),
+                  size: screenWidth * 0.14,
+                ),
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.03,
+              right: screenWidth * 0.04,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  GestureDetector(
+                    onTap: () async {
+                      final prefs = await SharedPreferences.getInstance();
+                      final code = prefs.getString('userReferralCode') ?? 'No referral code yet';
+                      Share.share(code);
+                    },
+                    child: Container(
+                      width: screenWidth * 0.128,
+                      height: screenWidth * 0.128,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.white, width: 3),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withValues(alpha: 0.15),
+                            blurRadius: 10,
+                            offset: const Offset(0, 7),
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.share_rounded,
+                          color: Colors.black,
+                          size: screenWidth * 0.07,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: screenWidth * 0.03),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const NotificationScreen(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      width: screenWidth * 0.128,
+                      height: screenWidth * 0.128,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.white, width: 3),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withValues(alpha: 0.15),
+                            blurRadius: 10,
+                            offset: const Offset(0, 7),
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.notifications_none_rounded,
+                          color: Colors.black,
+                          size: screenWidth * 0.07,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.04,
+              left: screenWidth * 0.04,
+              child: GestureDetector(
+                onTap: () => Navigator.of(context).maybePop(),
+                child: Container(
+                  width: screenWidth * 0.13,
+                  height: screenWidth * 0.13,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF8C2B0),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.chevron_left,
+                      color: Colors.black,
+                      size: screenWidth * 0.10,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
